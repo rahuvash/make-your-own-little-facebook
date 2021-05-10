dddb8cb7196f1f26f6cc8edfe42fc59c19939cde
rahuvash <77118888+rahuvash@users.noreply.github.com>
2021-05-10 16:25:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var database=[
+{
+	username: "rahul vashisth"
+	password: "rahuvas"
+}
+];
+
+var newsfeed =[
+{
+username :"bobby",
+timeline:"so tired from all this learning!"
+},
+{
+	username : "selly",
+	timeline: "javascript is so cool!"
+}
+];
+
+var userNamePrompt = prompt("whats your username?");
+var passwordPrompt = prompt("whats your password?");
+
+function signIn(user, password)
+{
+	if (user===database[0].username) &&
+		pass===database[0].password)
+ console.log(newsfeed);
+
+ else
+ {
+ 	alert("sorry , wrong username and password");
+ }
+}
